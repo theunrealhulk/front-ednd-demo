793d8bf0d02884b9942544580e157dc4d0869bbd
JC <chafiaay@protonmail.com>
2021-08-02 06:44:25 +0100

    bootstrap implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <Header       
+        headding="L'UNIVERS CLEANZY" 
+        subheadding="Rejoignez-nous sur les reseaux sociaux!"/>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = ({headding,subheadding}) => {
+    return (
+        <div className='container'>
+            <h1>{headding}</h1>
+            <h6>{subheadding}</h6>
+        </div>
+    )
+}
+
+export default Header
